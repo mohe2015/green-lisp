5117f34a1e4da2b2ca550f4432b8a1f724c0c85e
mohe2015 <mohe2015@users.noreply.github.com>
2019-10-08 15:24:57 +0200

    cffi

--- src/serial-interface.lisp
+++ src/serial-interface.lisp
@@ -5,3 +5,12 @@
 (define-foreign-library libserial-interface
   (t (:default "libserial-interface")))
 (use-foreign-library libserial-interface)
+
+(defcfun "serial_connect" :pointer)
+(defcfun "serial_read" :int
+  (handle :pointer))
+(defcfun "serial_write" :bool
+  (handle :pointer)
+  (char :char))
+(defcfun "serial_close" :void
+  (handle :pointer))
