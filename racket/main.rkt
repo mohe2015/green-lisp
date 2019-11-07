1b8d7eac9e22a0d6e7119a410d53958560db8ee3
mohe2015 <mohe2015@users.noreply.github.com>
2019-11-07 12:22:34 +0100

    Switch to untyped

--- racket/main.rkt
+++ racket/main.rkt
@@ -1,39 +1,31 @@
-#lang typed/racket/base
+#lang racket/base
 ;; https://docs.racket-lang.org/ts-guide/
-(module unsafe typed/racket/base
 
-  ;; https://docs.racket-lang.org/binary-class/index.html
-  (require typed/racket/class)
-  (require/typed binary-class
-                 [binary (-> (-> Input-Port Any) (-> Output-Port Integer Any) Any)])
-
-  ;; http://webassembly.github.io/spec/core/binary/conventions.html
-  (define (valtype)
-    (binary
-     (λ ([in : Input-Port]) (read-byte in))
-     (λ ([out : Output-Port] [value : Integer]) (write-byte value out))))
-
-  (define-binary-class id3-tag%
-    ((major-version  u1)
-     (revision       u1)
-     (flags          u1))
-    #:dispatch 
-    nil)
+(provide read-module)
   
-  (define-binary-class module%
-    ((_ (constant (list #x00 #x61 #x73 #x6D)))))
+;; https://docs.racket-lang.org/binary-class/index.html
+(require racket/class)
+(require binary-class)
+  
+;; http://webassembly.github.io/spec/core/binary/conventions.html
+(define (valtype)
+  (binary
+   (λ (in) (read-byte in))
+   (λ (out value) (write-byte value out))))
 
-  (define (read-module file)
-    (call-with-input-file file
-      (λ (in) (read-value module% in))))
+(define-binary-class module%
+  ((_ (constant #"\0asm"))))
 
-  ;; TODO https://docs.racket-lang.org/optimization-coach/index.html
+(define (read-module file)
+  (call-with-input-file file
+    (λ (in) (read-value module% in))))
 
-  ;; https://docs.racket-lang.org/guide/i_o.html
-  (: write-file (-> Output-Port Any))
-  (define (write-file out)
-    (write-byte 1 out))
+;; TODO https://docs.racket-lang.org/optimization-coach/index.html
 
-  (call-with-output-file "data" #:exists 'truncate write-file))
+;; https://docs.racket-lang.org/guide/i_o.html
+(define (write-file out)
+  (write-byte 1 out))
 
-  ;; /home/moritz/Documents/green-lisp/webassembly/demo/example.wasm
+(call-with-output-file "data" #:exists 'truncate write-file)
+
+;; /home/moritz/Documents/green-lisp/webassembly/demo/example.wasm
