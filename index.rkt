718c49e673bcebf5e94cb7ce383ffca4629da5e2
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-28 20:10:51 +0100

    Implement writer

--- index.rkt
+++ index.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require (only-in racket/base (write-byte racket-write-byte)))
 
 (define bit-writer%
   (class object%
@@ -13,10 +14,28 @@
       (for ((i (in-range 7 -1 -1)))
         (write-bit (if (bitwise-bit-set? byte i) 1 0))))
 
+    (define/public (write-to-file file)
+      (call-with-output-file file
+        (lambda (out)
+          (let ((bits (reverse bits)))
+            (for ((byte-index (in-range 0 (length bits) 8)))
+              (let ((byte 0))
+                (for ((bit-index (in-range 8)))
+                  (set! byte (bitwise-ior byte (arithmetic-shift (car bits) bit-index)))
+                  (displayln byte)
+                  (set bits (cdr bits)))
+                (racket-write-byte byte out)))))
+          #:mode 'binary #:exists 'truncate/replace))
+    
     (define/public (get-bits)
       (reverse bits))))
 
+(define (jmp writer displacement)
+  (send writer write-byte #xeb)
+  (send writer write-byte displacement))
+
+
 (let ((writer (new bit-writer%)))
-  (send writer write-byte 1)
-  (send writer write-byte 2)
-  (send writer get-bits))
+  (jmp writer #xfe)
+  (send writer get-bits)
+  (send writer write-to-file "/tmp/a.bin"))
