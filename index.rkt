00fa8bd3a33fb882fd89c58b9e3bb9648d3be9ae
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-28 22:30:40 +0100

    Small fix

--- index.rkt
+++ index.rkt
@@ -10,6 +10,29 @@
     (define/public (write-bit bit)
       (set! bits (cons bit bits)))
 
+    (define/public (write-unsigned-16 integer)
+      (let ((value (integer->integer-bytes integer 2 #f)))
+        (write-byte (bytes-ref value 0)) ;; TODO big / little endian
+        (write-byte (bytes-ref value 1))))
+
+    (define/public (write-unsigned-32 integer)
+      (let ((value (integer->integer-bytes integer 4 #f)))
+        (write-byte (bytes-ref value 0))
+        (write-byte (bytes-ref value 1))
+        (write-byte (bytes-ref value 2))
+        (write-byte (bytes-ref value 3))))
+
+    (define/public (write-unsigned-64 integer)
+      (let ((value (integer->integer-bytes integer 8 #f)))
+        (write-byte (bytes-ref value 0))
+        (write-byte (bytes-ref value 1))
+        (write-byte (bytes-ref value 2))
+        (write-byte (bytes-ref value 3))
+        (write-byte (bytes-ref value 4))
+        (write-byte (bytes-ref value 5))
+        (write-byte (bytes-ref value 6))
+        (write-byte (bytes-ref value 7))))
+
     (define/public (write-byte byte)
       (for ((i (in-range 7 -1 -1)))
         (write-bit (if (bitwise-bit-set? byte i) 1 0))))
@@ -97,19 +120,41 @@
 (define EM_X86_64 64)
 
 (let ((writer (new bit-writer%)))
+  ;; header size 56
   ;; e_ident
-  (send writer write-byte ELFMAG0)
   (send writer write-byte ELFMAG0)
   (send writer write-byte ELFMAG1)
   (send writer write-byte ELFMAG2)
+  (send writer write-byte ELFMAG3)
   (send writer write-byte ELFCLASS64)
   (send writer write-byte ELFDATA2LSB)
   (send writer write-byte EV_CURRENT)
   (send writer write-byte ELFOSABI_SYSV)
-  (send writer write-byte ET_EXEC) ;; e_type
-  (send writer write-byte EM_X86_64) ;; e_machine
-  (send writer write-byte EV_CURRENT) ;; e_version
-  (send writer write-byte 1) ;; entrypoint) ;; e_entry
+  (for ((i (in-range 8)))
+    (send writer write-byte 0))
+  (send writer write-unsigned-16 ET_EXEC) ;; e_type
+  (send writer write-unsigned-16 EM_X86_64) ;; e_machine
+  (send writer write-unsigned-32 EV_CURRENT) ;; e_version
+  (send writer write-unsigned-64 106) ;; aTODO entrypoint) ;; e_entry
+  (send writer write-unsigned-64 32) ;; e_phoff aTODO phdr - $$
+  (send writer write-unsigned-64 0) ;; e_shoff
+  (send writer write-unsigned-32 0) ;; e_flags
+  (send writer write-unsigned-16 56) ;; e_ehsize aTODO headersize
+  (send writer write-unsigned-16 50) ;; e_phentsize aTODO phdrsize
+  (send writer write-unsigned-16 1) ;; e_phnum
+  (send writer write-unsigned-16 0) ;; e_shentsize
+  (send writer write-unsigned-16 0) ;; e_shnum
+  (send writer write-unsigned-16 0) ;; e_shstrndx
+
+  ;; phdr Elf64_Phdr size 50
+  (send writer write-byte 1) ;; p_type
+  (send writer write-byte 5) ;; p_flags
+  (send writer write-unsigned-64 0) ;; p_offset
+  (send writer write-unsigned-64 64) ;; p_vaddr aTODO current addr
+  (send writer write-unsigned-64 72) ;; p_paddr aTODO current addr
+  (send writer write-unsigned-64 108) ;; p_filesz aTODO filesize
+  (send writer write-unsigned-64 108) ;; p_memsz aTODO filesize
+  (send writer write-unsigned-64 #x1000) ;; p_align
   
   (jmp writer -2) ;; size of jmp instruction
   (send writer get-bits)
