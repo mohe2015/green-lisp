3d3804c52d5ebc4b8a2e2fbcae4e6fc4a33ebfc4
dev.mohe <Moritz.Hedtke@t-online.de>
2020-03-29 14:46:51 +0200

    Fix

--- index.rkt
+++ index.rkt
@@ -10,30 +10,30 @@
     (define/public (write-bit bit)
       (set! bits (cons bit bits)))
 
-    (define/public (write-unsigned-2 integer)
+    (define/public (write-unsigned-16 integer)
       (let ((value (integer->integer-bytes integer 2 #f)))
-        (write-unsigned-1 (bytes-ref value 0)) ;; TODO big / little endian
-        (write-unsigned-1 (bytes-ref value 1))))
+        (write-unsigned-8 (bytes-ref value 0)) ;; TODO big / little endian
+        (write-unsigned-8 (bytes-ref value 1))))
 
-    (define/public (write-unsigned-4 integer)
+    (define/public (write-unsigned-32 integer)
       (let ((value (integer->integer-bytes integer 4 #f)))
-        (write-unsigned-1 (bytes-ref value 0))
-        (write-unsigned-1 (bytes-ref value 1))
-        (write-unsigned-1 (bytes-ref value 2))
-        (write-unsigned-1 (bytes-ref value 3))))
+        (write-unsigned-8 (bytes-ref value 0))
+        (write-unsigned-8 (bytes-ref value 1))
+        (write-unsigned-8 (bytes-ref value 2))
+        (write-unsigned-8 (bytes-ref value 3))))
 
-    (define/public (write-unsigned-8 integer)
+    (define/public (write-unsigned-64 integer)
       (let ((value (integer->integer-bytes integer 8 #f)))
-        (write-unsigned-1 (bytes-ref value 0))
-        (write-unsigned-1 (bytes-ref value 1))
-        (write-unsigned-1 (bytes-ref value 2))
-        (write-unsigned-1 (bytes-ref value 3))
-        (write-unsigned-1 (bytes-ref value 4))
-        (write-unsigned-1 (bytes-ref value 5))
-        (write-unsigned-1 (bytes-ref value 6))
-        (write-unsigned-1 (bytes-ref value 7))))
+        (write-unsigned-8 (bytes-ref value 0))
+        (write-unsigned-8 (bytes-ref value 1))
+        (write-unsigned-8 (bytes-ref value 2))
+        (write-unsigned-8 (bytes-ref value 3))
+        (write-unsigned-8 (bytes-ref value 4))
+        (write-unsigned-8 (bytes-ref value 5))
+        (write-unsigned-8 (bytes-ref value 6))
+        (write-unsigned-8 (bytes-ref value 7))))
 
-    (define/public (write-unsigned-1 byte)
+    (define/public (write-unsigned-8 byte)
       (for ((i (in-range 7 -1 -1)))
         (write-bit (if (bitwise-bit-set? byte i) 1 0))))
 
@@ -53,12 +53,12 @@
       (reverse bits))))
 
 (define (jmp writer displacement)
-  (send writer write-unsigned-1 #xeb)
-  (send writer write-unsigned-1 (bytes-ref (integer->integer-bytes displacement 1 #t) 0)))
+  (send writer write-unsigned-8 #xeb)
+  (send writer write-unsigned-8 (bytes-ref (integer->integer-bytes displacement 1 #t) 0)))
 
 (define (mov-imm8 writer register value)
-  (send writer write-unsigned-1 (bitwise-ior #xb0 register))
-  (send writer write-unsigned-1 value))
+  (send writer write-unsigned-8 (bitwise-ior #xb0 register))
+  (send writer write-unsigned-8 value))
 
 ;; https://github.com/torvalds/linux/blob/master/include/uapi/linux/elf.h
 ;;typedef struct elf64_hdr {
@@ -133,41 +133,41 @@
   ;; e_ident
   ;; file start
   ;; ehdr start
-  (send writer write-unsigned-1 ELFMAG0)
-  (send writer write-unsigned-1 ELFMAG1)
-  (send writer write-unsigned-1 ELFMAG2)
-  (send writer write-unsigned-1 ELFMAG3)
-  (send writer write-unsigned-1 ELFCLASS64)
-  (send writer write-unsigned-1 ELFDATA2LSB)
-  (send writer write-unsigned-1 EV_CURRENT)
-  (send writer write-unsigned-1 ELFOSABI_SYSV)
+  (send writer write-unsigned-8 ELFMAG0)
+  (send writer write-unsigned-8 ELFMAG1)
+  (send writer write-unsigned-8 ELFMAG2)
+  (send writer write-unsigned-8 ELFMAG3)
+  (send writer write-unsigned-8 ELFCLASS64)
+  (send writer write-unsigned-8 ELFDATA2LSB)
+  (send writer write-unsigned-8 EV_CURRENT)
+  (send writer write-unsigned-8 ELFOSABI_SYSV)
   (for ((i (in-range 8)))
-    (send writer write-unsigned-1 0))
-  (send writer write-unsigned-2 ET_EXEC) ;; e_type
-  (send writer write-unsigned-2 EM_X86_64) ;; e_machine
-  (send writer write-unsigned-4 EV_CURRENT) ;; e_version
-  (send writer write-unsigned-8 (+ base 120)) ;; aTODO entrypoint) ;; e_entry
-  (send writer write-unsigned-8 64) ;; e_phoff aTODO phdr - $$
-  (send writer write-unsigned-8 0) ;; e_shoff
-  (send writer write-unsigned-4 0) ;; e_flags
-  (send writer write-unsigned-2 64) ;; e_ehsize aTODO headersize
-  (send writer write-unsigned-2 56) ;; e_phentsize aTODO phdrsize
+    (send writer write-unsigned-8 0))
+  (send writer write-unsigned-16 ET_EXEC) ;; e_type
+  (send writer write-unsigned-16 EM_X86_64) ;; e_machine
+  (send writer write-unsigned-32 EV_CURRENT) ;; e_version
+  (send writer write-unsigned-64 (+ base 120)) ;; aTODO entrypoint) ;; e_entry
+  (send writer write-unsigned-64 64) ;; e_phoff aTODO phdr - $$
+  (send writer write-unsigned-64 0) ;; e_shoff
+  (send writer write-unsigned-32 0) ;; e_flags
+  (send writer write-unsigned-16 64) ;; e_ehsize aTODO headersize
+  (send writer write-unsigned-16 56) ;; e_phentsize aTODO phdrsize
 
-  (send writer write-unsigned-2 1) ;; e_phnum p
-  (send writer write-unsigned-2 0) ;; e_shentsize
-  (send writer write-unsigned-2 0) ;; e_shnum p
-  (send writer write-unsigned-2 0) ;; e_shstrndx
+  (send writer write-unsigned-16 1) ;; e_phnum p
+  (send writer write-unsigned-16 0) ;; e_shentsize
+  (send writer write-unsigned-16 0) ;; e_shnum p
+  (send writer write-unsigned-16 0) ;; e_shstrndx
   ;; ehdr end 64
 
   ;; phrd start
-  (send writer write-unsigned-4 1) ;; p_type
-  (send writer write-unsigned-4 5) ;; p_flags ;; read + execute
-  (send writer write-unsigned-8 0) ;; p_offset
-  (send writer write-unsigned-8 base) ;; p_vaddr aTODO current addr
-  (send writer write-unsigned-8 base) ;; p_paddr aTODO current addr
-  (send writer write-unsigned-8 (+ 120 code-size)) ;; p_filesz aTODO filesize
-  (send writer write-unsigned-8 (+ 120 code-size)) ;; p_memsz aTODO filesize
-  (send writer write-unsigned-8 #x1000) ;; p_align
+  (send writer write-unsigned-32 1) ;; p_type
+  (send writer write-unsigned-32 5) ;; p_flags ;; read + execute
+  (send writer write-unsigned-64 0) ;; p_offset
+  (send writer write-unsigned-64 base) ;; p_vaddr aTODO current addr
+  (send writer write-unsigned-64 base) ;; p_paddr aTODO current addr
+  (send writer write-unsigned-64 (+ 120 code-size)) ;; p_filesz aTODO filesize
+  (send writer write-unsigned-64 (+ 120 code-size)) ;; p_memsz aTODO filesize
+  (send writer write-unsigned-64 #x1000) ;; p_align
   ;; phrd end 56
 
   ;; code start 120 until here
@@ -176,18 +176,18 @@
   ;; mov     rsi, string ; string1 to source index
   (mov-imm8 writer 0 1)  ; al / rax: set write to command
 
-  (send writer write-unsigned-1 #x40) ; REX prefix to access dil instead of bh
+  (send writer write-unsigned-8 #x40) ; REX prefix to access dil instead of bh
   (mov-imm8 writer 7 1)  ; bh / dil / rdi: set destination index to rax (stdout)
   ;; syscall 
   
   (jmp writer -2) ;; size of jmp instruction
 
-  (send writer write-unsigned-1 (char->integer #\H))
-  (send writer write-unsigned-1 (char->integer #\e))
-  (send writer write-unsigned-1 (char->integer #\l))
-  (send writer write-unsigned-1 (char->integer #\l))
-  (send writer write-unsigned-1 (char->integer #\o))
-  (send writer write-unsigned-1 (char->integer #\!))
+  (send writer write-unsigned-8 (char->integer #\H))
+  (send writer write-unsigned-8 (char->integer #\e))
+  (send writer write-unsigned-8 (char->integer #\l))
+  (send writer write-unsigned-8 (char->integer #\l))
+  (send writer write-unsigned-8 (char->integer #\o))
+  (send writer write-unsigned-8 (char->integer #\!))
   ;; code end 2
   ;; file end 122
 
