d01a995bc4976d797bf7812383c68717c39b5fe9
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-05 14:27:06 +0200

    Idea

--- test-utils.rkt
+++ test-utils.rkt
@@ -15,3 +15,14 @@
              (values
               (append cara cdra)
               (append carb cdrb))))]))
+
+'(data-list
+  (data-align 12)
+  (label symbol))
+;; I think this may work as the expressions don't get longer and longer that way
+'(let* ((start 0)
+        (element-0 (+ start     ((lambda (current-address) (get-byte-count-to-align alignment-bits current-address)) start)))
+        (element-1 (+ element-0 ((lambda (_) 0) element-0)))
+        (symbol element-1))
+   ((lambda (current-address) (make-bytes (get-byte-count-to-align alignment-bits current-address) 0)) elment-0)
+   ((lambda (current-address) (bytes)) element-1))
