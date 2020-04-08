b87abe5e0b7f8dd2eb2071f2363a59cc5843c819
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-08 21:23:33 +0200

    Jo

--- new-x86-64.rkt
+++ new-x86-64.rkt
@@ -9,13 +9,25 @@
     (integer->integer-bytes value (arithmetic-shift bits -3) #f)))
 
 (define-syntax-rule (data-unsigned bytes value)
-  (list (lambda (_) bytes) null (lambda (current-address) (integer->integer-bytes value bytes #f))))
+  (list (lambda (_) bytes)
+        null
+        (lambda (current-address) (integer->integer-bytes value bytes #f))
+        (list)
+        ))
 
 (define-syntax-rule (data-bytestring bytestring)
-  (list (lambda (_) (bytes-length bytestring)) null (lambda (_) bytestring)))
+  (list (lambda (_) (bytes-length bytestring))
+        null
+        (lambda (_) bytestring)
+        (list)
+        ))
 
 (define-syntax-rule (data-filled-array size)
-  (list (lambda (_) size) null (lambda (_) (make-bytes size 0))))
+  (list (lambda (_) size)
+        null
+        (lambda (_) (make-bytes size 0))
+        (list)
+        ))
 
 (define (get-byte-count-to-align alignment-bits offset)
   (- (arithmetic-shift (arithmetic-shift (+ offset (arithmetic-shift 1 alignment-bits) -1) (- alignment-bits)) alignment-bits) offset))
@@ -23,17 +35,30 @@
 (define-syntax-rule (data-align alignment-bits)
   (list (lambda (current-address) (get-byte-count-to-align alignment-bits current-address))
         null
-        (lambda (current-address) (make-bytes (get-byte-count-to-align alignment-bits current-address) 0))))
+        (lambda (current-address) (make-bytes (get-byte-count-to-align alignment-bits current-address) 0))
+        (list)
+        ))
 
 (define-syntax-rule (label symbol)
-  (list (lambda (_) 0) symbol (lambda (current-address) (bytes))))
+  (list (lambda (_) 0)
+        symbol
+        (lambda (current-address) (bytes))
+        (list)
+        ))
 
 ;; https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=224&zoom=100,28,726
 (define-syntax-rule (call target)
-  (list (lambda (_) 5) null (lambda (current-address) (bytes-append (bytes #xe8) (integer->integer-bytes (- target current-address 5) 4 #t)))))
+  (list (lambda (_) 5)
+        null
+        (lambda (current-address) (bytes-append (bytes #xe8) (integer->integer-bytes (- target current-address 5) 4 #t)))
+        (list)
+        ))
 
 (define-syntax-rule (syscall)
-  (list (lambda (_) 2) null (lambda (_) (bytes #x0f #x05))))
+  (list (lambda (_) 2)
+        null
+        (lambda (_) (bytes #x0f #x05))
+        (list)))
 
 (define-syntax-rule (mov-imm8 register value)
   (list (lambda (_) (if (= register 7) 3 2))
@@ -43,7 +68,9 @@
            (if (= the-register 7)
                (unsigned 8 #x40)
                (bytes)) ; REX prefix to access dil instead of bh
-           (bytes (bitwise-ior #xb0 the-register) (dynamic the-value))))))
+           (bytes (bitwise-ior #xb0 the-register) (dynamic the-value))))
+        (list)
+        ))
 
 (define-syntax-rule (mov-imm64 register value)
   (list (lambda (_) 10)
@@ -52,81 +79,46 @@
           (bytes-append
            (unsigned 8 REX.W)
            (unsigned 8 (+ #xb8 register)) ;; opcode with register
-           (unsigned 64 value))))) ;; value
-
-;; alternative proposal
-'(define-method test (jo)
-   "hi")
-;; returns a function object
-;; has fields like symbols (test, jo), code, data (), rodata ("hi"), size, .bss ()
-;; compiler gets those and combines them
-;; can also optimize by moving around things etc., dead code elimination, etc,
-;; then it calls the objects get-bytes with data map as parameter from which get-bytes can get the addresses (what about PIE?)
-
-;; macroexpanding, ... before all that, automated code rewriting, ..., inlining
-
-;; but still source code location needs to be preserved so maybe store it inside of the objects
-
-;; lambda macro should return object that accepts address and returns the code for that lambda
-;; locally resolves labels
-;; maybe labels not even required in some cases because they can be optimized away before
-
-;; should be PIE code so the offset should be irrelevant?
-;; except for strings its currently not absolute
-
-;; TODO conditionals
-;; (if (= a 1) 1 0)
-;; converted into cmp, ... by the if macro?
-
-;; variable itself are hard to implement...
-;; with macros? - check how (maybe stack with currently live variables), but functional...
-
-;; jumps to symbols are hard to implement (global jumps to other function, local jumps for loops etc.) - functional...
-
-;; memory allocation:
-;; https://linux.die.net/man/2/mmap2
-;; https://linux.die.net/man/2/mremap
-;; (new test-object%)
-;; -> (call allocate-bytes 10)
-;; check for 0
-;; (initialize-test-object pointer)
-;; done
-
-;; http://matt.might.net/articles/implementing-a-programming-language/
-
-;; but maybe this is the quick proposal? (no, do it right the first time)
+           (unsigned 64 value)))
+        (list)
+        )) ;; value
 
 (define-syntax-rule (mov-string register value)
   (list (lambda (_) 10)
         null
-        (lambda (current-address data-addresses rodata-addresses)
+        (lambda (current-address rodata-addresses)
           (bytes-append
            (unsigned 8 REX.W)
            (unsigned 8 (+ #xb8 register)) ;; opcode with register
-           (unsigned 64 (first data-addresses))))
-        (list value) ;; .data
-        (list value) ;; rodata
-        )) ;; value
+           (unsigned 64 (first rodata-addresses))))
+        (list value) ;; .rodata
+        ))
 
 (define-syntax-rule (jmp target)
   (list (lambda (_) 2)
         null
         (lambda (current-address)
-          (bytes-append (bytes #xeb) (integer->integer-bytes (- target current-address 2) 1 #t)))))
+          (bytes-append (bytes #xeb) (integer->integer-bytes (- target current-address 2) 1 #t)))
+        (list)
+        ))
 
 ;; https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=1163&zoom=100,-7,754
 (define-syntax-rule (push register)
   (list (lambda (_) 1)
         null
         (lambda (_)
-          (bytes (+ #x50 register)))))
+          (bytes (+ #x50 register)))
+        (list)
+        ))
 
 ;; https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=1037&zoom=auto,-17,727
 (define-syntax-rule (pop register)
   (list (lambda (_) 1)
         null
         (lambda (_)
-          (bytes (+ #x58 register)))))
+          (bytes (+ #x58 register)))
+        (list)
+        ))
 ;; (bytes-append (bytes #x8f) (integer->integer-bytes the-register 1 #f)))
  
 ;; https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=133&zoom=100,-7,726
@@ -135,10 +127,12 @@
         null
         (lambda (_)
           ;; https://www.intel.com/content/dam/www/public/us/en/documents/manuals/64-ia-32-architectures-software-developer-instruction-set-reference-manual-325383.pdf#page=40&zoom=100,28,745
-          (bytes REX.W 01 (mod11-to-binary destination source)))))
+          (bytes REX.W 01 (mod11-to-binary destination source)))
+        (list)
+        ))
 
 (begin-for-syntax
-  (define (list->label-addresses symbols sizes codes offset)
+  (define (list->label-addresses symbols sizes codes rodatas offset)
     (cond [(or (null? symbols) (null? sizes) (null? codes)) (values (list) (list))]
           [else
            (let* ([current-element-symbol (car (generate-temporaries '(element)))] ;; syntax element
@@ -152,7 +146,7 @@
                                            (values
                                             (list (list current-element-symbol offset) (list symbol current-element-symbol))
                                             `(, #`(#,code #,current-element-symbol))))]
-                          [(cdra cdrb) (list->label-addresses (cdr symbols) (cdr sizes) (cdr codes) #`(+ #,current-element-symbol #,size))])
+                          [(cdra cdrb) (list->label-addresses (cdr symbols) (cdr sizes) (cdr codes) (cdr rodatas) #`(+ #,current-element-symbol #,size))])
                (values
                 (append cara cdra)
                 (append carb cdrb))))])))
@@ -166,8 +160,9 @@
             (sizes (map (lambda (c) (second c)) expanded)) ;; syntax list of all sizes
             (symbols (map (lambda (c) (third c)) expanded)) ;; syntax list of all symbols
             (codes (map (lambda (c) (fourth c)) expanded)) ;; syntax list of all codes
+            (rodatas (map (lambda (c) (fifth c)) expanded)) ;; syntax list of .rodata
             (tainted (map (lambda (c) (syntax-tainted? c)) sizes)))
-       (let-values ([(labels code) (list->label-addresses symbols sizes codes 0)])
+       (let-values ([(labels code) (list->label-addresses symbols sizes codes rodatas 0)])
          #`(let* #,labels (bytes-append #,@code))))]))
 
 (define (get-the-code)
@@ -222,3 +217,48 @@
    (push 0)
 
    (label code-end)))
+
+
+
+
+;; alternative proposal
+;;'(define-method test (jo)
+;;   "hi")
+;; returns a function object
+;; has fields like symbols (test, jo), code, data (), rodata ("hi"), size, .bss ()
+;; compiler gets those and combines them
+;; can also optimize by moving around things etc., dead code elimination, etc,
+;; then it calls the objects get-bytes with data map as parameter from which get-bytes can get the addresses (what about PIE?)
+
+;; macroexpanding, ... before all that, automated code rewriting, ..., inlining
+
+;; but still source code location needs to be preserved so maybe store it inside of the objects
+
+;; lambda macro should return object that accepts address and returns the code for that lambda
+;; locally resolves labels
+;; maybe labels not even required in some cases because they can be optimized away before
+
+;; should be PIE code so the offset should be irrelevant?
+;; except for strings its currently not absolute
+
+;; TODO conditionals
+;; (if (= a 1) 1 0)
+;; converted into cmp, ... by the if macro?
+
+;; variable itself are hard to implement...
+;; with macros? - check how (maybe stack with currently live variables), but functional...
+
+;; jumps to symbols are hard to implement (global jumps to other function, local jumps for loops etc.) - functional...
+
+;; memory allocation:
+;; https://linux.die.net/man/2/mmap2
+;; https://linux.die.net/man/2/mremap
+;; (new test-object%)
+;; -> (call allocate-bytes 10)
+;; check for 0
+;; (initialize-test-object pointer)
+;; done
+
+;; http://matt.might.net/articles/implementing-a-programming-language/
+
+;; but maybe this is the quick proposal? (no, do it right the first time)
