50eb542bf174d8c31fcf1fb9173d08cc9b8bc53c
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-08 23:07:20 +0200

    Fix entrypoint

--- main.rkt
+++ main.rkt
@@ -1,4 +1,5 @@
 (module main racket
+  (require green-lisp/utils)
   (require green-lisp/elf/section)
   (require green-lisp/elf/program-header)
   (require green-lisp/elf/file)
@@ -7,33 +8,40 @@
   ;; Derived from: https://github.com/torvalds/linux/blob/master/include/uapi/linux/elf.h
   ;; Licensed under /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 
-  (let-values ([(.text .rodata) (get-the-code 0 #x400278)])
-    (let* ((.text-section (new elf-section%
-                               [name #".text"]
-                               [type 'progbits]
-                               [flags '(alloc exec)]
-                               [content .text]))
-           (.rodata-section (new elf-section%
-                                 [name #".rodata"]
+  ;; we could know the start of the sections before because we know the section count
+  ;; but only for the first section
+  ;; except when we map them in memory with a big distance
+  ;; otherwise we can't because of alignment etc.
+  ;; for the data we should try to use a constant address
+
+  (let ((rodata-base-address (+ BASE 128 (* 64 2) (* 56 2)))) ;; TODO change this design as there will be multiple sections
+    (let-values ([(.text .rodata) (get-the-code 0 rodata-base-address)])
+      (let* ((.text-section (new elf-section%
+                                 [name #".text"]
                                  [type 'progbits]
-                                 [flags '(alloc write)]
-                                 [content .rodata]))
-           (.text-program-header (new elf-program-header%
-                                      [type 'load]
-                                      [flags '(read execute)]
-                                      [section .text-section]
-                                      ))
-           (.rodata-program-header (new elf-program-header%
+                                 [flags '(alloc exec)]
+                                 [content .text]))
+             (.rodata-section (new elf-section%
+                                   [name #".rodata"]
+                                   [type 'progbits]
+                                   [flags '(alloc write)]
+                                   [content .rodata]))
+             (.text-program-header (new elf-program-header%
                                         [type 'load]
-                                        [flags '(read write)]
-                                        [section .rodata-section]
+                                        [flags '(read execute)]
+                                        [section .text-section]
                                         ))
-           (bytes (send (new elf-file%
-                             [sections (list .text-section .rodata-section)]
-                             [program-headers (list .text-program-header .rodata-program-header)]) get-bytes)))
-      (call-with-output-file "out.elf"
-        (lambda (out)
-          (write-bytes bytes out))
-        #:mode 'binary #:exists 'truncate/replace)
-      (file-or-directory-permissions "out.elf" (bitwise-ior user-read-bit user-write-bit user-execute-bit))))
+             (.rodata-program-header (new elf-program-header%
+                                          [type 'load]
+                                          [flags '(read write)]
+                                          [section .rodata-section]
+                                          ))
+             (bytes (send (new elf-file%
+                               [sections (list .rodata-section .text-section)]
+                               [program-headers (list .rodata-program-header .text-program-header)]) get-bytes)))
+        (call-with-output-file "out.elf"
+          (lambda (out)
+            (write-bytes bytes out))
+          #:mode 'binary #:exists 'truncate/replace)
+        (file-or-directory-permissions "out.elf" (bitwise-ior user-read-bit user-write-bit user-execute-bit)))))
   )
