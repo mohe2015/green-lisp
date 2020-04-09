457e2278aef8247fea747f4566e722ab3ae39ef9
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-09 22:25:30 +0200

    Jo

--- elf/file.rkt
+++ elf/file.rkt
@@ -86,19 +86,25 @@
                                           [name #".strtab"]
                                           [type 'strtab]
                                           [content symbols-string-table-bytes]))
+
+               (symbols-table-bytes (map (lambda (symbol)
+                                           (send symbol get-bytes
+                                                 ;; TODO section index
+                                                 (send symbols-string-table get-string-offset (get-field name symbol))
+                                                 ))
+                                         symbols))
+               (symbols-table-section (new elf-section%
+                                           [name #".symtab"]
+                                           [type 'symtab]
+                                           [content symbols-table-bytes]))
                
-               ;(symbol-table-section (new elf-section%
-               ;                           [name #".symtab"]
-               ;                           [type 'symtab]
-               ;                           [content symbols-string-table-bytes]))
-                             
-               (section-header-string-table (new elf-string-table% [strings (cons #".strtab" (cons #".shstrtab" (map (lambda (section) (get-field name section)) sections)))]))
+               (section-header-string-table (new elf-string-table% [strings (cons #".strtab" (cons #".symtab" (cons #".shstrtab" (map (lambda (section) (get-field name section)) sections))))]))
                (section-header-string-table-bytes (send section-header-string-table get-bytes))
                (section-header-string-table-section (new elf-section%
                                                          [name #".shstrtab"]
                                                          [type 'strtab]
                                                          [content section-header-string-table-bytes]))
-               (new-elf-file (merge (new elf-file% [sections (list section-header-string-table-section symbols-string-table-section)]))))
+               (new-elf-file (merge (new elf-file% [sections (list section-header-string-table-section symbols-string-table-section symbols-table-section)]))))
           (send new-elf-file internal-get-bytes section-header-string-table)))
 
       (define/public (get-elf-header-bytes) ;; 64 bytes
