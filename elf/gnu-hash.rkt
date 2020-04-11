de758ac90692b7edd08e2909e95df4c0dd6bf9bd
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-11 23:15:21 +0200

    Add notes

--- elf/gnu-hash.rkt
+++ elf/gnu-hash.rkt
@@ -24,10 +24,42 @@
 ;; https://www.gabriel.urdhr.fr/2015/09/28/elf-file-format/#hash-tables
 ;; struct Gnu_Hash_Header {
 ;;  uint32_t nbuckets;
-;;  uint32_t symndx;    /* Index of the first accessible symbol in .dynsym */
-;;  uint32_t maskwords; /* Nyumber of elements in the Bloom Filter */
-;;  uint32_t shift2;    /* Shift count for the Bloom Filter */
-;;  uintXX_t bloom_filter[maskwords];
+;;  uint32_t symndx / symoffset;    /* Index of the first accessible symbol in .dynsym */
+;;  uint32_t maskwords / bloom_size; /* Nyumber of elements in the Bloom Filter */
+;;  uint32_t shift2 / bloom_shift;    /* Shift count for the Bloom Filter */
+;;  uintXX_t bloom_filter/bloom[maskwords/bloom_size]; // 64 / 32
 ;;  uint32_t buckets[nbuckets];
-;;  uint32_t values[dynsymcount - symndx];
+;;  uint32_t values[dynsymcount - symndx] / chain[];
 ;;};
+
+
+;; bloom[(hash / ELFCLASS_BITS) % bloom_size]
+;; TODO just set every bit so it is ignored first
+;; if bit hash % ELFCLASS_BITS and bit (hash >> bloom_shift) % ELFCLASS_BITS
+;; is set the symbol may be in the hash table, otherwise it isn't
+
+
+;; we have to skip the symbols which are not global etc.
+
+;; symbol placed in hash % nbuckets bucket
+
+;; last bit in chain set -> end of chain
+
+;; bucket array holds index for first symbol in chain (bucket[foobar] - symoffset])
+
+;; Chains being contiguous sequences imply that symbols within the same bucket must be stored contiguously.
+
+;; A bucket element will contain the index 0 if there is no symbol in the hash table for the given value of N. As index 0 of the dynsym is a reserved value, this index cannot occur for a valid symbol, and is therefore non-ambiguous.
+
+;; sort the symbols before based on their hash?
+
+;; TODO these values need to be calculated somehow
+;; nbuckets = 4
+;; symoffset = 1 because of undefined symbol
+;; bloom_size 2
+;; bloom_shift 5
+
+;; https://flapenguin.me/elf-dt-gnu-hash
+
+;; https://blogs.oracle.com/solaris/gnu-hash-elf-sections-v2 may explain the bloom filter more accurate
+;; on the other hand wikipedia should also contain information about the bloom filter
