b6cad965d9492197066b970d1050ee60383e7629
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-13 13:28:52 +0200

    Jo

--- cpp/test.sh
+++ cpp/test.sh
@@ -1,9 +1,11 @@
 # https://www.nesono.com/sites/default/files/lldb%20cheat%20sheet.pdf
 LD_LIBRARY_PATH=.. lldb main
+settings set target.disable-aslr false
+settings show target.disable-aslr
 breakpoint set --name main
 breakpoint set --name green_lisp_demo
 run
-
+stepi
 
 help breakpoint set
 breakpoint set -a 0x555555555020
@@ -18,6 +20,7 @@ image dump symtab libgreen-lisp.so
 
 
 LD_LIBRARY_PATH=.. gdb main
+set disable-randomization off
 # https://sourceware.org/gdb/wiki/ReverseDebug
 start
 target record-full
