9c65152c8e91b870d7afb9aa56d327932c052780
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-14 22:32:42 +0200

    This works

--- x86-64-new/index.rkt
+++ x86-64-new/index.rkt
@@ -1,5 +1,17 @@
 (module index racket
-  (provide (rename-out (module-begin #%module-begin))) ;; #%module-begin #%app #%datum #%top
+  (provide #%datum)
+  (provide #%top)
+  (provide #%app)
+  (provide (rename-out [module-begin #%module-begin]))
 
+  ;; https://docs.racket-lang.org/guide/module-languages.html
+  
+  (begin-for-syntax
+    (define abc
+      (lambda (test)
+        3)))
+  
   (define-syntax (module-begin stx)
-    (raise-syntax-error #f "no" stx)))
+    ;(raise-syntax-error #f "no" stx)))
+    (abc stx)
+    #'(#%module-begin 2)))
