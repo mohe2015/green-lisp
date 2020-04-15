adc89bd4befdc8908028d9d7fbabedfb4a8f4363
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-15 19:38:34 +0200

    Fuck you

--- x86-64-new/index.rkt
+++ x86-64-new/index.rkt
@@ -5,10 +5,11 @@
   (provide #%top-interaction)
   (provide (rename-out [module-begin #%module-begin]))
   (require (for-syntax syntax/parse))
-
+  (require syntax/parse)
+  
   ;; https://docs.racket-lang.org/guide/module-languages.html
 
-  ;; (raise-syntax-error #f "no" test)
+  ;; 
   ;; https://docs.racket-lang.org/syntax/stxparse-patterns.html
   ;; https://docs.racket-lang.org/syntax/Parsing_Syntax.html
   ;; https://docs.racket-lang.org/reference/stxops.html
@@ -20,14 +21,16 @@
     
     (define evaluate
       (lambda (expression environment)
+        (println expression)
         (syntax-parse expression
-          [x:id (cond
+          [x (cond
                   [(symbol? (syntax-e #'x))
                    (environment-lookup environment expression)]
                   [(number? (syntax-e #'x))
-                   #`2]
-
-
+                   (println #`1337)
+                   #`1337]
+                  [else (raise-syntax-error #f "unknown syntax" #'x)]
+                  
                   )
                 ]
           )
