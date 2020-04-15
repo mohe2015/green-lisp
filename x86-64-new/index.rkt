eb12da0cf3f5bcac2b2a0de2d6faf0bde933225f
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-15 18:37:36 +0200

    Jo

--- x86-64-new/index.rkt
+++ x86-64-new/index.rkt
@@ -9,17 +9,31 @@
   ;; https://docs.racket-lang.org/guide/module-languages.html
 
   ;; (raise-syntax-error #f "no" test)
+  ;; https://docs.racket-lang.org/syntax/stxparse-patterns.html
+  ;; https://docs.racket-lang.org/syntax/Parsing_Syntax.html
+  ;; https://docs.racket-lang.org/reference/stxops.html
   
   (begin-for-syntax
+    (define environment-lookup
+      (lambda (environment expression)
+        #`1))
+    
     (define evaluate
       (lambda (expression environment)
         (syntax-parse expression
-          [x:id #`(quote 1)]
+          [x:id (cond
+                  [(symbol? (syntax-e #'x))
+                   (environment-lookup environment expression)]
+                  [(number? (syntax-e #'x))
+                   #`(push #,#'x)]
 
+
+                  )
+                ]
           )
         )
       )
     )
-  ;; #,(evaluate #`stx `())
+  
   (define-syntax (module-begin stx)
-    #`(#%module-begin 1)))
+    #`(#%module-begin #,(evaluate #`stx `()))))
