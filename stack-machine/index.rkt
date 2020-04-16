fd51959b46bbc317333ca00c4dae6f377de4f737
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-16 13:56:32 +0200

    Jo

--- stack-machine/index.rkt
+++ stack-machine/index.rkt
@@ -1,14 +1,10 @@
-(module stack-machine/index racket/base
+(module stack-machine/index racket
   (require racket/match)
 
   (define (compile expression environment)
     (match expression
-      [(? number?) `(push ,expression)]
-
-
+      [(? number?) `((push ,expression))]
       [`(let ,bindings ,body) (compile-let bindings body environment)]
-
-
       ))
 
   (define (compile-with environment) 
@@ -19,7 +15,12 @@
            (expressions (map cadr bindings))
            (values (map (compile-with environment) expressions))
            (environment* (env-extend* environment variables values)))
-      (compile body environment*)))
+      `(,@(append* values)
+        ,@(compile body environment*) ;; should return value at current position in stack
+        (set! r1 (pop))
+        (popn ,(length values))
+        (push r1)
+        )))
 
   (define (env-empty) (hash))
 
@@ -33,8 +34,8 @@
        ; =>
        env]))
 
-  (define-struct cell ([value #:mutable]))
+  (define-struct cell ([location #:mutable]))
   
-  (compile '(let ((a 1)) 1) (env-empty))
+  (compile '(let ((a 1)) 2) (env-empty))
 
   )
