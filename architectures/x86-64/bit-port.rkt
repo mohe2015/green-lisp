ea682e1018b2e26700bee0195a67e8d1d6835ab5
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-17 16:43:50 +0200

    Implement bit-writer

--- architectures/x86-64/bit-port.rkt
+++ architectures/x86-64/bit-port.rkt
@@ -1,4 +1,4 @@
-(module bit-port racket/base
+(module bit-port racket
 
   (struct bit-port (port pending-bits)
     #:transparent #:mutable)
@@ -8,10 +8,24 @@
 
     )
 
+  (define (bits->byte bits)
+    (cond [(empty? bits) 0]
+          [(empty? (rest bits)) (car bits)]
+          [else (+ (arithmetic-shift (car bits) (length (cdr bits))) (bits->byte (cdr bits)))]))
+  
   (define (write-bit bit-port bit)
     (set-bit-port-pending-bits! bit-port
                                 (cons bit
-                                      (bit-port-pending-bits bit-port))))
+                                      (bit-port-pending-bits bit-port)))
+    (when (= 8 (length (bit-port-pending-bits bit-port)))
+      (write-byte (bits->byte (bit-port-pending-bits bit-port)) (bit-port-port bit-port))
+      (set-bit-port-pending-bits! bit-port '())))
+
+  (define (get-bytes bit-port)
+    (when (not (empty? (bit-port-pending-bits bit-port)))
+      (error "pending bits (not full byte)"))
+    (get-output-bytes (bit-port-port bit-port))
+    )
 
   (let ((in (open-input-bytes #"Hidffs")))
 
@@ -20,7 +34,14 @@
   (let* ((original-out (open-output-bytes))
          (out (bit-port original-out '())))
     (write-bit out 1)
-    (get-output-bytes original-out)
+    (write-bit out 1)
+    (write-bit out 1)
+    (write-bit out 1)
+    (write-bit out 1)
+    (write-bit out 1)
+    (write-bit out 1)
+    (write-bit out 1)
+    (get-bytes out)
     )
   
   )
