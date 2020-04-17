f1e9c1e5a2c99e6afee84988ef1d2517718140ba
dev.mohe <Moritz.Hedtke@t-online.de>
2020-04-17 20:24:19 +0200

    Jo

--- architectures/x86-64/low-level.rkt
+++ architectures/x86-64/low-level.rkt
@@ -1,11 +1,22 @@
 (module low-level racket/base
-
+  (require green-lisp/architectures/x86-64/bit-port)
+  
   ;(define-instruction x86-64-call
   ;  (_                (constant-bits '(1 1 1 0 1 0 0 0)))
   ;  (relative-address (signed-integer 4)))
 
-  (struct x86-64-call (relative-address))
-    
+  ;;https://docs.racket-lang.org/binary-class/index.html
   
+  (struct binary (read write))
+  
+  (struct constant-bits-data (bits))
+  
+  (define constant-bits
+    (binary
+     (lambda (in)
+       null)
+     (lambda (out)
+       null)))
+     
   
   )
